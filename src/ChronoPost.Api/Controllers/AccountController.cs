325c3f78a7a9e7cf1ae2f6fdba41f39baac7dce7
Fatih Gencaslan <fatih.gencaslan@adesso.com.tr>
2023-10-03 22:56:42 +0300

    Create get user by id

--- src/ChronoPost.Api/Controllers/AccountController.cs
+++ src/ChronoPost.Api/Controllers/AccountController.cs
@@ -1,3 +1,5 @@
+using ChronoPost.UseCases.User.FindUserById;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChronoPost.Api.Controllers;
@@ -6,10 +8,17 @@ namespace ChronoPost.Api.Controllers;
 [Route("api/account")]
 public class AccountController : ControllerBase
 {
+    private readonly IMediator _mediator;
+
+    public AccountController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
 
     [HttpGet("{id:int}")]
-    public IActionResult Get(int id)
+    public async Task<IActionResult> Get(int id)
     {
-        throw new NotImplementedException();
+        var result = await _mediator.Send(new FindUserByIdQuery(id));
+        return Ok(result);
     }
 }
