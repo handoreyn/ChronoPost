73488bfb6f5dd7889329b8f96246dfea9acf1668
handoreyn <fatih.gencaslan@adesso.com.tr>
2023-10-08 23:22:34 +0300

    repo mock fix

--- tests/ChronoPost.UseCaseTests/User/UserQueryTests.cs
+++ tests/ChronoPost.UseCaseTests/User/UserQueryTests.cs
@@ -8,29 +8,26 @@ namespace ChronoPost.UseCaseTests.User;
 [TestFixture]
 public class UserQueryTests
 {
-    private IReadRepository<Core.Aggregates.User> _readRepository;
+    private readonly Mock<IReadRepository<Core.Aggregates.User>> _readRepositoryMock = new();
 
     [SetUp]
     public void FindUserById_UserDoesExist_Setup()
     {
-        var mock = new Mock<IReadRepository<Core.Aggregates.User>>();
         var user = new Core.Aggregates.User
         {
             Id = 1,
             UserCredentials = new Core.ValueObjects.UserCredentialValueObject("testuser", "password"),
         };
 
-        mock.Setup(o => o.GetByIdAsync(1, CancellationToken.None))
+        _readRepositoryMock.Setup(o => o.GetByIdAsync(1, CancellationToken.None))
             .ReturnsAsync(user);
-
-        _readRepository = mock.Object;
     }
 
     [Test]
     [TestCase(1)]
     public async Task FindUserById_UserDoesExist(int id)
     {
-        var handler = new FindUserByIdQueryHandler(_readRepository);
+        var handler = new FindUserByIdQueryHandler(_readRepositoryMock.Object);
         var result = await handler.Handle(new FindUserByIdQuery(id), CancellationToken.None);
         Assert.That(result, Is.Not.Null);
 
@@ -39,7 +36,7 @@ public class UserQueryTests
     [TestCase(0)]
     public void FindUserById_UserDoesNotExist(int userId)
     {
-        var handler = new FindUserByIdQueryHandler(_readRepository);
+        var handler = new FindUserByIdQueryHandler(_readRepositoryMock.Object);
         Assert.CatchAsync<UserDoesNotExistException>(async () =>
         {
             await handler.Handle(new FindUserByIdQuery(userId), CancellationToken.None);
