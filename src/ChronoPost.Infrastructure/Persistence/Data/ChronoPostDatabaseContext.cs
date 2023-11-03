da5a0ba16cd40e7ea09ce37560e0f8a718ca5326
handoreyn <fatih.gencaslan@adesso.com.tr>
2023-11-03 15:41:45 +0300

    savepoint

--- src/ChronoPost.Infrastructure/Persistence/Data/ChronoPostDatabaseContext.cs
+++ src/ChronoPost.Infrastructure/Persistence/Data/ChronoPostDatabaseContext.cs
@@ -1,3 +1,4 @@
+using Ardalis.SharedKernel;
 using ChronoPost.Core.Aggregates;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,10 +6,12 @@ namespace ChronoPost.Infrastructure.Persistence.Data;
 
 public class ChronoPostDatabaseContext : DbContext
 {
-    public ChronoPostDatabaseContext(DbContextOptions options) : base(options)
+    private readonly IDomainEventDispatcher _dispatcher;
+    public ChronoPostDatabaseContext(DbContextOptions options, IDomainEventDispatcher dispatcher) : base(options)
     {
         Database.EnsureCreated();
         Database.Migrate();
+        _dispatcher = dispatcher;
     }
 
     public required DbSet<User> Users { get; set; }
@@ -18,5 +21,25 @@ public class ChronoPostDatabaseContext : DbContext
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+        if (_dispatcher == null) return result;
+
+        var entities = ChangeTracker.Entries<EntityBase>()
+            .Select(o => o.Entity)
+            .Where(o => o.DomainEvents.Any())
+            .ToArray();
+
+        await _dispatcher.DispatchAndClearEvents(entities);
+        return result;
+    }
+
+    public override int SaveChanges()
+    {
+        return SaveChangesAsync().GetAwaiter().GetResult();
     }
 }
--- src/ChronoPost.UseCases/Users/Commands/CreateUser/CreateUserCommandHandler.cs
+++ src/ChronoPost.UseCases/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -1,22 +1,27 @@
 using Ardalis.SharedKernel;
+using ChronoPost.Core.Aggregates.Events;
 using ChronoPost.Core.Enums;
 using ChronoPost.Core.Specifications.User;
+using MediatR;
 
 namespace ChronoPost.UseCases.Users.Commands.CreateUser;
 
 public sealed class CreateUserCommandHandler : ICommandHandler<CreateUserCommand, CreateUserCommandResponse>
 {
     private readonly IRepository<Core.Aggregates.User> _repository;
+    private readonly IPublisher _publisher;
 
-    public CreateUserCommandHandler(IRepository<Core.Aggregates.User> repository)
+    public CreateUserCommandHandler(IRepository<Core.Aggregates.User> repository, IPublisher publisher)
     {
         _repository = repository;
+        _publisher = publisher;
     }
 
     public async Task<CreateUserCommandResponse> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
         var userByUsernameOrEmailAddressSpecification = new UserByUserNameOrEmailSpecification(request.Username, request.Email);
-        var user = await _repository.FirstOrDefaultAsync(userByUsernameOrEmailAddressSpecification);
+        var user = await _repository.FirstOrDefaultAsync(userByUsernameOrEmailAddressSpecification, cancellationToken);
+
         if (user is not null) throw new Exception("User does exist.");
         user = new Core.Aggregates.User
         {
@@ -25,6 +30,8 @@ public sealed class CreateUserCommandHandler : ICommandHandler<CreateUserCommand
             Status = StatusType.Active,
             CreatedAt = DateTime.UtcNow,
         };
+
+        user.RegisterEvent(new UserRegisteredEvent(user.Email, user.UserCredentials.Username));
 
         await _repository.AddAsync(user, cancellationToken);
         await _repository.SaveChangesAsync(cancellationToken);
